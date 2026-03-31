a3353f78aa235d5e357f04e5f67e3361272ac813
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2026-03-31 22:24:07 +0700

    update

---
+++ src/components/ChildLifeCycle.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Child life cycle</h1>
+</template>
+<script>
+export default {
+  name: "ChildLifeCycle",
+  mounted() {
+    this.hello = setInterval(() => {
+      console.log("ChildLifeCycle is running");
+    }, 1000);
+  },
+  beforeUnmount() {
+    clearInterval(this.hello);
+    console.log("ChildLifeCycle is unmounted");
+  },
+  unmounted() {
+    console.log("ChildLifeCycle is unmounted");
+  },
+};
+</script>
+<style></style>
--- src/components/DemoLifeCycle.vue
+++ src/components/DemoLifeCycle.vue
@@ -1,15 +1,22 @@
 <template>
   <h1>Demo life cycle</h1>
 
-  <input type="text" />
+  <input ref="myInput" type="text" />
 
   <p>{{ count }}</p>
 
   <button class="btn btn-success" @click="handleIncrement">Increment</button>
+
+  <ChildLifeCycle v-if="count % 2 === 0" />
 </template>
 <script>
+import ChildLifeCycle from "./ChildLifeCycle.vue";
+
 export default {
   name: "DemoLifeCycle",
+  components: {
+    ChildLifeCycle,
+  },
   data() {
     return {
       count: 0,
@@ -27,6 +34,19 @@ export default {
   created() {
     console.log("created", this.count);
     this.handleIncrement();
+  },
+  beforeMount() {
+    console.log("beforeMount", this.$refs.myInput);
+  },
+  mounted() {
+    console.log("mounted", this.$refs.myInput);
+    this.$refs.myInput.focus();
+  },
+  beforeUpdate() {
+    console.log("Loading = true");
+  },
+  updated() {
+    console.log("Loading = false");
   },
 };
 </script>
